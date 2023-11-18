94e1ef304d76f572ec570bc45d1783f1010fca03
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2023-11-18 13:50:52 +0700

    first version of 18/11/2023

--- src/User.java
+++ src/User.java
@@ -1,4 +1,16 @@
 public class User {
-    
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(String id, String firstName, String lastName, String email, String password){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
     
 }
