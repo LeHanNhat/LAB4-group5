e8a4fd6e00e2a3d2cd2a09d781e9a9b2dac68886
tranphuquycubin <131520832+tranphuquycubin@users.noreply.github.com>
2023-11-25 14:26:00 +0700

    abc

--- src/User.java
+++ src/User.java
@@ -1,5 +1,8 @@
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.security.SecureRandom;
 
 public class User {
 
@@ -19,32 +22,6 @@ public class User {
 		this.LastName = lastName;
 		this.Email = email;
 		this.Password = password;
-	}
-
-	public void Register() {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("User registered successfully!");
-		System.out.println("User Registration:");
-
-		System.out.print("Enter First Name: ");
-		String firstName = scanner.nextLine();
-
-		System.out.print("Enter Last Name: ");
-		String lastName = scanner.nextLine();
-
-		System.out.print("Enter Email: ");
-		String email = scanner.nextLine();
-
-		System.out.print("Enter Password: ");
-		String password = scanner.nextLine();
-
-		System.out.println("User Registered Successfully:");
-		System.out.println("First Name: " + firstName);
-		System.out.println("Last Name: " + lastName);
-		System.out.println("Email: " + email);
-		System.out.println("Password: " + password);
-
 	}
 
 	public void Login() {
@@ -72,63 +49,162 @@ public class User {
 	public void UpdateProfile() {
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("User Profile Update:");
+		System.out.println("User Profile Update:");
 
-        System.out.print("Enter New First Name (leave blank to keep current): ");
-        String newFirstName = scanner.nextLine();
-        if (!newFirstName.isEmpty()) {
-            storedFirstName = newFirstName;
-        }
+		System.out.print("Enter New First Name (leave blank to keep current): ");
+		String newFirstName = scanner.nextLine();
+		isNameValid(newFirstName);
+		if (!newFirstName.isEmpty()) {
+			storedFirstName = newFirstName;
+		}
 
-        System.out.print("Enter New Last Name (leave blank to keep current): ");
-        String newLastName = scanner.nextLine();
-        if (!newLastName.isEmpty()) {
-            storedLastName = newLastName;
-        }
+		System.out.print("Enter New Last Name (leave blank to keep current): ");
+		String newLastName = scanner.nextLine();
+		isNameValid(newLastName);
+		if (!newLastName.isEmpty()) {
+			storedLastName = newLastName;
+		}
 
-        System.out.print("Enter New Email (leave blank to keep current): ");
-        String newEmail = scanner.nextLine();
-        if (!newEmail.isEmpty()) {
-            storedEmail = newEmail;
-        }
+		System.out.print("Enter New Email (leave blank to keep current): ");
+		String newEmail = scanner.nextLine();
+		isValidEmail(newEmail);
+		if (!newEmail.isEmpty()) {
+			storedEmail = newEmail;
+		}
 
-        System.out.print("Enter New Password (leave blank to keep current): ");
-        String newPassword = scanner.nextLine();
-        if (!newPassword.isEmpty()) {
-            storedPassword = newPassword;
-        }
+		System.out.print("Enter New Password (leave blank to keep current): ");
+		String newPassword = scanner.nextLine();
+		verifyPassword(newPassword);
+		if (!newPassword.isEmpty()) {
+			storedPassword = newPassword;
+		}
 
-        System.out.println("User Profile Updated Successfully:");
-        System.out.println("First Name: " + storedFirstName);
-        System.out.println("Last Name: " + storedLastName);
-        System.out.println("Email: " + storedEmail);
-        System.out.println("Password: " + storedPassword);
-		
+		System.out.println("User Profile Updated Successfully:");
+		System.out.println("First Name: " + storedFirstName);
+		System.out.println("Last Name: " + storedLastName);
+		System.out.println("Email: " + storedEmail);
+		System.out.println("Password: " + storedPassword);
+
 	}
 
 	public void ResetPassword() {
 
 		Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Password Reset:");
+		System.out.println("Password Reset:");
 
-        System.out.print("Enter your Email: ");
-        String userEmail = scanner.nextLine();
+		System.out.print("Enter your Email: ");
+		String userEmail = scanner.nextLine();
 
-        if (userEmail.equals(storedEmail)) {
-            String newPassword = generateTemporaryPassword();
+		if (userEmail.equals(storedEmail)) {
+			String newPassword = RandomPasswordGenerator.generatePassword();
 
-            storedPassword = newPassword;
+			storedPassword = newPassword;
 
-            System.out.println("Password reset successful! Your new temporary password is: " + newPassword);
-        } else {
-            System.out.println("Password reset failed. Invalid email.");
-        }
+			System.out.println("Password reset successful! Your new temporary password is: " + newPassword);
+		} else {
+			System.out.println("Password reset failed. Invalid email.");
+		}
 
-    }
-
-    private static String generateTemporaryPassword() {
-        return "newTemporaryPassword123";
 	}
 
+	public static boolean isValidEmail(String email) {
+		String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+		Pattern pattern = Pattern.compile(emailRegex);
+		Matcher matcher = pattern.matcher(email);
+
+		return matcher.matches();
+	}
+
+	public static boolean verifyPassword(String password) {
+		if (password.length() < 8) {
+			return false;
+		}
+
+		if (!password.matches(".*\\d.*")) {
+			return false;
+		}
+
+		if (!password.matches(".*[A-Z].*")) {
+			return false;
+		}
+
+		Pattern specialChars = Pattern.compile("[^a-zA-Z0-9]");
+		Matcher matcher = specialChars.matcher(password);
+		if (!matcher.find()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public static boolean isNameValid(String name) {
+		for (char c : name.toCharArray()) {
+			if (Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
+
+class RandomPasswordGenerator {
+
+	private static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+	private static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String NUMBERS = "0123456789";
+	private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[{]}|;:,<.>/?";
+
+	public static String generatePassword() {
+		SecureRandom random = new SecureRandom();
+
+		StringBuilder password = new StringBuilder();
+
+		password.append(UPPERCASE_LETTERS.charAt(random.nextInt(UPPERCASE_LETTERS.length())));
+
+		password.append(NUMBERS.charAt(random.nextInt(NUMBERS.length())));
+
+		password.append(SPECIAL_CHARACTERS.charAt(random.nextInt(SPECIAL_CHARACTERS.length())));
+
+		int remainingLength = 5;
+		for (int i = 0; i < remainingLength; i++) {
+			int type = random.nextInt(3);
+			switch (type) {
+				case 0:
+					password.append(LOWERCASE_LETTERS.charAt(random.nextInt(LOWERCASE_LETTERS.length())));
+					break;
+				case 1:
+					password.append(UPPERCASE_LETTERS.charAt(random.nextInt(UPPERCASE_LETTERS.length())));
+					break;
+				case 2:
+					password.append(NUMBERS.charAt(random.nextInt(NUMBERS.length())));
+					break;
+				case 3:
+					password.append(SPECIAL_CHARACTERS.charAt(random.nextInt(SPECIAL_CHARACTERS.length())));
+					break;
+			}
+		}
+
+		String shuffledPassword = shuffleString(password.toString());
+		return shuffledPassword;
+	}
+
+	private static String shuffleString(String input) {
+		char[] characters = input.toCharArray();
+		SecureRandom random = new SecureRandom();
+		for (int i = 0; i < characters.length; i++) {
+			int randomIndex = random.nextInt(characters.length);
+			char temp = characters[randomIndex];
+			characters[randomIndex] = characters[i];
+			characters[i] = temp;
+		}
+		return new String(characters);
+	}
+
+	public static void main(String[] args) {
+		String generatedPassword = generatePassword();
+		System.out.println("Generated Password: " + generatedPassword);
+	}
 }
